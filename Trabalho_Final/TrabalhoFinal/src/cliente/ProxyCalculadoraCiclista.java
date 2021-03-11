e0e5426bead87dbb5c073ee62c9e6a1aa458533e
Rafaelsurfce <Rafaelsurfce@alu.ufc.br>
2021-03-11 16:14:18 -0300

    30%

---
+++ Trabalho_Final/TrabalhoFinal/src/cliente/ProxyCalculadoraCiclista.java
@@ -0,0 +1,33 @@
+package cliente;
+
+import java.io.IOException;
+
+public class ProxyCalculadoraCiclista {
+	UDPCliente cliente=new UDPCliente();
+	String ip= "localhost";
+	int porta=1515;
+	
+	
+	public String IMC(double altura, double peso) throws IOException {
+		String dados="IMC"+"-"+altura+"-"+peso;
+		cliente.sendRequest(dados, porta, ip);
+		return cliente.sendResponse();
+	}
+	
+	public String alturaBike(double altura) throws IOException {
+		String dados="alturaBike"+"-"+altura;
+		cliente.sendRequest(dados, porta, ip);
+		return cliente.sendResponse();
+	}
+	
+	public String batimentosCardiacos(int batimentosTotal) throws IOException {
+		String dados="batimentosCardiacos"+"-"+batimentosTotal;
+		cliente.sendRequest(dados, porta, ip);
+		return cliente.sendResponse();
+	}
+	
+	public void close() {
+		cliente.close();
+	}
+
+}
---
+++ Trabalho_Final/TrabalhoFinal/src/cliente/UDPCliente.java
@@ -0,0 +1,37 @@
+package cliente;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class UDPCliente {
+	
+	DatagramSocket socket=null;
+	public void sendRequest(String request, int porta, String ip) throws IOException {
+		socket= new DatagramSocket();
+		byte[] dados=request.getBytes();
+		InetAddress host=InetAddress.getByName(ip);
+		int servePorta=porta;
+		DatagramPacket mensagem=new DatagramPacket(dados, dados.length, host, servePorta);
+		socket.send(mensagem);
+		System.out.println("mensagem enviada");
+		
+	}
+	
+	public String sendResponse() throws IOException {
+		byte[] buffer=new byte[1000];
+		DatagramPacket resposta=new DatagramPacket(buffer, buffer.length);
+		socket.receive(resposta);
+		System.out.println("mensagem recebida");
+		String result=new String (resposta.getData());
+		return result; 
+	}
+	
+	public void close() {
+		socket.close();
+	}
+
+}
